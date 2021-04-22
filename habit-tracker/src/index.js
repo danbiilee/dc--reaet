a942cf13e5690a0e2f01702d3f4ea5e7ecce6825
danbiilee <danbi.db@gmail.com>
2021-04-22 21:26:31 +0900

    Add SimpleHabit

--- habit-tracker/src/index.js
+++ habit-tracker/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js';
+import SimpleHabit from './components/simpleHabit';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <SimpleHabit />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
