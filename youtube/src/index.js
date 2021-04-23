fbafceb3512393fced600a35be0309963efa1d36
danbiilee <danbi.db@gmail.com>
2021-04-23 15:33:33 +0900

    [PKG] Add styled-components, fontawesome

--- youtube/src/index.js
+++ youtube/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import GlobalStyle from './utils/globalStyle';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<GlobalStyle />
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
