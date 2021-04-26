014493ce3607761ec6a61938bdef41843360e1a8
danbiilee <danbi.db@gmail.com>
2021-04-27 08:33:05 +0900

    add youtube service

--- youtube-ellie/src/app.jsx
+++ youtube-ellie/src/app.jsx
@@ -1,25 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+/* 
+  💩💩💩 고칠 점 
+  1. api_key 숨김 처리
+  2. 네트워크 통신 로직 분리 
+    - class에 통신에 관한 로직을 모아놓고, 필요할 때 컴포넌트에 주입(dependency injection)
+    - unit test를 해야할 경우, 테스트를 할 때마다 네트워크 통신이 되어야하는 것은 최악! 
+      -> test할 땐 mock data가 들어있는 class를 전달
+  
+  💡 결론: MVC 패턴의 View에 충실한 (멍청한) React Component 만들기
+*/
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
+  };
 
-    fetch(
-      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBjIAYGsiQrBH95bOoyt8cNbo7kKX5-7F8',
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
 
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
--- youtube-ellie/src/index.js
+++ youtube-ellie/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import './index.css';
+import Youtube from './service/youtube';
+
+// index.js가 호출될 때 딱 한번만 만들어짐!
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
   document.getElementById('root')
 );
