5fff2752d8ab03fb02a7e22972b37bb2e200d2c6
danbiilee <danbi.db@gmail.com>
2021-04-26 19:32:50 +0900

    show video list with title

--- youtube-ellie/src/app.jsx
+++ youtube-ellie/src/app.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>This is template!</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBjIAYGsiQrBH95bOoyt8cNbo7kKX5-7F8',
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
