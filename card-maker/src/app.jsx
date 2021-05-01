ac770bb1f8fc33dac0644986cb4735da3fbdb76a
danbiilee <danbi.db@gmail.com>
2021-05-01 12:50:45 +0900

    apply ellie version

--- card-maker/src/app.jsx
+++ card-maker/src/app.jsx
@@ -1,45 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import './app.css';
-import Login from './page/login';
+import styles from './app.module.css';
+import Login from './components/login/login';
 
-function App() {
-  const [user, setUser] = useState(null);
-  console.log(user);
-
-  const providers = {
-    google: new window.firebase.auth.GoogleAuthProvider(),
-    github: new window.firebase.auth.GithubAuthProvider(),
-  };
-
-  const handleSignIn = type => {
-    window.firebase.auth().signInWithRedirect(providers[type]);
-  };
-
-  const handleSignOut = () => {
-    window.firebase
-      .auth() //
-      .signOut()
-      .then(setUser(null))
-      .catch(console.log);
-  };
-
-  useEffect(() => {
-    window.firebase
-      .auth()
-      .getRedirectResult()
-      .then(result => {
-        console.log(result);
-        setUser(result.user);
-      })
-      .catch(error => console.log(error.message));
-  }, []);
-
+function App({ authService }) {
   return (
-    <Login
-      user={user}
-      handleSignIn={handleSignIn}
-      handleSignOut={handleSignOut}
-    />
+    <div className={styles.app}>
+      <Login authService={authService} />
+    </div>
   );
 }
 
--- card-maker/src/components/header/header.jsx
+++ card-maker/src/components/header/header.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import styles from './header.module.css';
 
-const Header = ({ user, handleSignOut }) => {
-  return (
-    <header className={styles.header}>
-      <img className={styles.logo} src="images/logo.png" alt="logo" />
-      <h1 className={styles.title}>Business Card Maker</h1>
-      {user && (
-        <button className={styles.button} onClick={handleSignOut}>
-          Logout
-        </button>
-      )}
-    </header>
-  );
-};
+const Header = ({ onLogout }) => (
+  <header className={styles.header}>
+    {onLogout && (
+      <button className={styles.logout} onClick={onLogout}>
+        Logout
+      </button>
+    )}
+    <img className={styles.logo} src="/images/logo.png" alt="logo" />
+    <h1 className={styles.title}>Business Card Maker</h1>
+  </header>
+);
 
 export default Header;
