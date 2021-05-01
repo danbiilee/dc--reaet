53bc728e1d0d5f8d82bcddfa6b6b6b584c86f745
danbiilee <danbi.db@gmail.com>
2021-05-01 12:51:57 +0900

    add app css

--- card-maker-ellie/src/index.js
+++ card-maker-ellie/src/index.js
@@ -1,7 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
-import AuthService from "./service/auth_service";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import './index.module.css';
+import AuthService from './service/auth_service';
 
 const authService = new AuthService();
 
@@ -9,5 +10,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App authService={authService} />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
