c093ba34a95f424f3cf8d6e1aa70e194189f46c4
danbiilee <danbi.db@gmail.com>
2021-05-12 18:07:05 +0900

    improve firebase code

--- card-maker-ellie/src/service/firebase.js
+++ card-maker-ellie/src/service/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -6,5 +8,12 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
+// Initialize Firebase: firebase.app.App
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-export default firebaseApp;
+
+export const firebaseAuth = firebaseApp.auth();
+export const firebaseDatabase = firebaseApp.database();
+
+// ⚡ firebase.app.App(firebaseApp) 안에는 provider가 없어서 추가로 제공해야 함
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const githubProvider = new firebase.auth.GithubAuthProvider();
